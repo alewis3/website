8c82129ff0ca88b12dddf7bd509835c7cab2115c
Amanda <alewis3@stedwards.edu>
2019-11-04 13:04:15 -0600

    Changed routes

--- react-express-app/app.js
+++ react-express-app/app.js
@@ -27,8 +27,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/', indexRouter);
+app.use('/api/users', usersRouter);
 
 app.use('public', express.static(path.join(__dirname, 'client/public')));
 
