96c663a2ee17cea80ae4bc458f07382eaeaeab1c
Amanda <alewis3@stedwards.edu>
2019-11-06 01:14:57 -0600

    Added / to path

--- react-express-app/routes/index.js
+++ react-express-app/routes/index.js
@@ -4,12 +4,12 @@ var path = require("path");
 
 if(process.env.NODE_ENV === 'production') {
     router.get('/', function (req, res) {
-         res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+         res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
     });
   }
   else {
     router.get('/', (req, res) => {  
-      res.sendFile(path.join(__dirname+'../client/public/index.html'));
+      res.sendFile(path.join(__dirname+'/../client/public/index.html'));
     });
   }
 
