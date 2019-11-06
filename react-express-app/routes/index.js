b50689617f2d46fbe95d0bf5ddacaeed218ff7d1
Amanda <alewis3@stedwards.edu>
2019-11-06 03:27:24 -0600

    Update for dev server to have favicon

--- react-express-app/routes/index.js
+++ react-express-app/routes/index.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var path = require("path");
 
 if(process.env.NODE_ENV === 'production') {
-    router.get('/', function (req, res) {
-         res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
-    });
-  }
-  else {
-    router.get('/', (req, res) => {  
-      res.sendFile(path.join(__dirname+'/../client/public/index.html'));
-    });
-  }
+  router.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
+  });
+}
+else {
+  router.get('/', (req, res) => {  
+    res.sendFile(path.join(__dirname+'/../client/public/index.html'));
+  });
+}
 
   module.exports = router;
