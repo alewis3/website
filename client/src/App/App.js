268b3feb2ffa58f8ee33b70e74d7e9491e210de5
Amanda Lewis <alewis3@stedwards.edu>
2020-01-26 19:20:16 -0600

    Changed App routing

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Resume from './pages/Resume';
 
 class App extends Component {
   render() {
     const App = () => (
       <div>
         <Switch>
+          <Route path='/resume' component={Resume}/>
           <Route exact path='/' component={Home}/>
         </Switch>
       </div>
