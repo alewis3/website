e73eda7f545933dd74cb32d5c8903a5cbdef94d9
Amanda Lewis <alewis3@stedwards.edu>
2020-01-30 00:51:09 -0600

    Routing for resume.html

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var path = require("path");
 
+router.get('/:file', function (req, res) {
+  res.sendFile(path.join(__dirname, '/../client/public/' + req.params.file));
+});
 if(process.env.NODE_ENV === 'production') {
   router.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
