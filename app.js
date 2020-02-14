043e4c9ebe1ed083caa777707ca0f5ebb71dedc9
Amanda Lewis <alewis3@stedwards.edu>
2020-02-14 13:43:21 -0600

    Added cors to project

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require('dotenv');
+var cors = require('cors');
 dotenv.config();
 
 var indexRouter = require('./routes/index');
@@ -24,6 +25,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
