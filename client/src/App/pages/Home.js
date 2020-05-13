97eb75659ab306a50ae2f042017068692ebe9159
Amanda Lewis <alewis3@stedwards.edu>
2020-05-12 21:55:14 -0500

    Changed up the bio

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -12,13 +12,13 @@ import MadLibs from './../components/MadLibs';
 
 class Home extends Component {
   render() {
-    const bio = "I am a Senior Computer Science major at St. Edward's University and I am interested in developing complex systems and learning more about backend frameworks like Python and Express apps written in Node.js. I currently have a 3.76 GPA and will graduate in May 2020 with a degree in Computer Science and a minor in Mathematics. In my off time I love to take my dog, Dana, on long walks by Lake Austin or to the dog park. I also enjoy playing guitar and learning new songs.";
+    const bio = "My name is Amanda, and I recently graduated from St. Edward's University with a 3.77 GPA and Magna Cum Laude honors. I am interested in developing complex systems and learning more about backend frameworks like Python and Express apps written in Node.js. I also am curious about frontend javascript frameworks like Vue with Nuxt. In my off time I love to take my dog, Dana, on long walks by Lake Austin or to the dog park. I also enjoy playing guitar and learning new songs."
     return (
     <div className="App">
       <Nav/>
       <div className="content">
       <Header src="/header.png" abt="SOFTWARE DESIGNER AND ENGINEER"/>
-      <Bio text={bio} srcme="/Grad.jpg" altme="Amanda Lewis" srcdana="/dana.png" altdana="dana"/>
+      <Bio text={bio} srcme="/Grad.jpg" altme="Amanda Lewis Graduate Photo" srcdana="/dana.png" altdana="dana"/>
       <ProjectSection/>
       <Awards/>
       <Table/>
